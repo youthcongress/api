97fc041b0e60726d531d6fdc166f9a83161996f6
Chetan Budathoki <chetanbudathoki@outlook.com>
2025-03-08 11:13:51 +0000

    start-server updates

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/youthcongress/api/database"
+	"github.com/youthcongress/api/server"
 	"github.com/youthcongress/api/storage"
 )
 
 func main(){
+
+	server.StartServer()
+
 	database.Connection()
 	storage.Connection()
+	
 }
