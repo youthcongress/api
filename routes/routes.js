ac99e44b6b0059cda95722fcb4588617a70b14ac
Chetan Budathoki <chetanbudathoki@outlook.com>
2025-03-21 14:36:48 +0000

    profile and login changes

--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,6 @@ app.use('/about', require('../scripts/about.js').app);
 app.use('/contact', require('../scripts/contact.js').app);
 app.use('/login', require('../scripts/login.js').app);
 app.use('/register', require('../scripts/register.js').app);
+app.use('/profile', require('../scripts/profile.js').app);
 
 module.exports = { app };
